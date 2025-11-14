fee2a83ff8f9c05d0ae1413614599fefa66b40ef
Matt H <contact@slightlyprivate.com>
2025-11-14 16:18:49 -0500

    feat: add web routes and documentation endpoint tests

---
+++ src/routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/**
+ * Web routes for public documentation UI.
+ * Scribe automatically registers docs routes when add_routes is enabled in config/scribe.php
+ */
