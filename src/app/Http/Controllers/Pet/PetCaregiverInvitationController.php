eed9477e8eab6088a4336236f8facb31cae9e02e
Matt H <contact@slightlyprivate.com>
2025-11-19 20:37:04 -0500

    feat: move PetCaregiverInvitationController for managing caregiver invitations

--- src/app/Http/Controllers/Pet/PetCaregiverInvitationController.php
+++ src/app/Http/Controllers/Pet/PetCaregiverInvitationController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Pet;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\AcceptPetCaregiverInvitationRequest;
 use App\Http\Requests\CreatePetCaregiverInvitationRequest;
 use App\Http\Requests\ListPetCaregiverInvitationsRequest;
