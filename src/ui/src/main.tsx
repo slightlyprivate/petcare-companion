ce3c403bb552d934958a8e30cbfc6819f17800b2
Matt H. <25968932+slightlyprivate@users.noreply.github.com>
2025-12-02 19:03:47 +0000

    chore: rename pwa/ui directories

--- src/ui/src/main.tsx
+++ src/ui/src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { getQueryClient } from './lib/queryClient';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const queryClient = getQueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
